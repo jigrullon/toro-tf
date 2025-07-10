20cf1e9fc9b7d9c4f8d08e1cc7be521b92462e15
jgrullon <jaimeigrullon@gmail.com>
2025-07-10 13:05:31 -0400

    Initial commit after some cleanup

---
+++ dns.tf
@@ -0,0 +1,18 @@
+# Lookup the hosted zone
+data "aws_route53_zone" "main" {
+  name         = "jaimegrullon.com"
+  private_zone = false
+}
+
+# Create/replace the A record to point to CloudFront
+resource "aws_route53_record" "root_alias" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "jaimegrullon.com"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.site.domain_name
+    zone_id                = aws_cloudfront_distribution.site.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
---
+++ main.tf
@@ -0,0 +1,99 @@
+provider "aws" {
+  region  = "us-east-1"
+  profile = "personal"
+}
+
+# ACM certificate for your domain
+resource "aws_acm_certificate" "site_cert" {
+  domain_name       = var.domain_name
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Route 53 zone lookup (assumes you use Route 53)
+data "aws_route53_zone" "zone" {
+  name         = var.domain_name
+  private_zone = false
+}
+
+# Validation record
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.site_cert.domain_validation_options : dvo.domain_name => {
+      name  = dvo.resource_record_name
+      type  = dvo.resource_record_type
+      value = dvo.resource_record_value
+    }
+  }
+
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.value]
+  ttl     = 60
+}
+
+# Certificate validation
+resource "aws_acm_certificate_validation" "cert_validation" {
+  certificate_arn         = aws_acm_certificate.site_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
+
+# CloudFront Origin Access Control (recommended over legacy OAI)
+resource "aws_cloudfront_origin_access_control" "s3_oac" {
+  name                              = "S3-OAC"
+  description                       = "Access control for S3 static site"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+# CloudFront distribution
+resource "aws_cloudfront_distribution" "site" {
+  origin {
+    domain_name = "${var.s3_bucket}.s3.amazonaws.com"
+    origin_id   = "s3-origin"
+    origin_access_control_id = aws_cloudfront_origin_access_control.s3_oac.id
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  aliases = [var.domain_name]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "s3-origin"
+
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn            = aws_acm_certificate_validation.cert_validation.certificate_arn
+    ssl_support_method             = "sni-only"
+    minimum_protocol_version       = "TLSv1.2_2021"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    Name = "MyPersonalSiteCDN"
+  }
+}
+
